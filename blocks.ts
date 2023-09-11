b01caa7aafa8dd68e5a8ca0675eb46957a4e7576
Alex Szeto <Alex.Szeto@gmail.com>
2023-09-10 21:40:49 -0700

    reorganize blocks

--- blocks.ts
+++ blocks.ts
@@ -85,97 +85,94 @@ namespace cardBuilder {
 
             this.margin = 2
             this.spacing = 1
-
-            this.resetZones()
         }
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% block="reset $this layout zones"
-        resetZones() {
-            this.rows = []
-            this.createNextRow()
-        }
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% block="reset $layout layout zones"
+    export function resetLayoutZones(layout: CardLayoutTemplate) {
+        layout.rows = []
+        editNextLayoutRow(layout)
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% block="edit $this next layout row"
-        createNextRow() {
-            this.rows.push(new cardKit.LayoutRow(CardZoneAlignments.Center, []))
-        }
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% block="edit $layout next layout row"
+    export function editNextLayoutRow(layout: CardLayoutTemplate) {
+        layout.rows.push(new cardKit.LayoutRow(CardZoneAlignments.Center, []))
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% block="align $this current row to $alignment"
-        changeRowAlignment(alignment: CardZoneAlignments) {
-            this.rows[this.rows.length - 1].align = alignment
-        }
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% block="align $layout current row to $alignment"
+    export function changeLayoutCurrentRowAlignment(layout: CardLayoutTemplate, alignment: CardZoneAlignments) {
+        layout.rows[layout.rows.length - 1].align = alignment
+    }
 
-        private addColumn(column: cardKit.LayoutColumn) {
-            this.rows[this.rows.length - 1].columns.push(column)
-        }
+    function addLayoutColumn(layout: CardLayoutTemplate, column: cardKit.LayoutColumn) {
+        layout.rows[layout.rows.length - 1].columns.push(column)
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% block="add $this empty space width $width height $height to current row"
-        createEmptySpaceZone(width: number, height: number) {
-            this.addColumn(cardKit.createEmptySpaceLayout(width, height))
-        }
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% block="add $layout empty space width $width height $height to current row"
+    export function createLayoutEmptySpaceZone(layout: CardLayoutTemplate, width: number, height: number) {
+        addLayoutColumn(layout, cardKit.createEmptySpaceLayout(width, height))
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% inlineInputMode=inline
-        //% block="add $this text $text in $color to current row|| limit line length $charsPerLine max lines $maxLines"
-        //% color.shadow="colorindexpicker" color.defl=15
-        //% charsPerLine.defl=0 maxLines.defl=1
-        createStaticTextZone(text: string, color: number, charsPerLine: number = 0, maxLines: number = 1) {
-            this.addColumn(cardKit.createTextLayout(text, color, charsPerLine <= 0 ? text.length : charsPerLine, maxLines))
-        }
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% inlineInputMode=inline
+    //% block="add $layout text $text in $color to current row|| limit line length $charsPerLine max lines $maxLines"
+    //% color.shadow="colorindexpicker" color.defl=15
+    //% charsPerLine.defl=0 maxLines.defl=1
+    export function createLayoutStaticTextZone(layout: CardLayoutTemplate, text: string, color: number, charsPerLine: number = 0, maxLines: number = 1) {
+        addLayoutColumn(layout, cardKit.createTextLayout(text, color, charsPerLine <= 0 ? text.length : charsPerLine, maxLines))
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% block="add $this image $image to current row"
-        createStaticImageZone(image: Image) {
-            this.addColumn(cardKit.createImageLayout(image))
-        }
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% block="add $layout image $image to current row"
+    export function createLayoutStaticImageZone(layout: CardLayoutTemplate, image: Image) {
+        addLayoutColumn(layout, cardKit.createImageLayout(image))
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% block="add $this card picture to current row"
-        createPictureZone() {
-            this.addColumn(cardKit.createPictureLayout())
-        }
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% block="add $layout card picture to current row"
+    export function createLayoutPictureZone(layout: CardLayoutTemplate, ) {
+        addLayoutColumn(layout, cardKit.createPictureLayout())
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% inlineInputMode=inline
-        //% block="add $this image $image to current row repeat number attribute $attribute times"
-        createRepeatedImageZone(attribute: string, image: Image) {
-            this.addColumn(cardKit.createAttributeAsRepeatImageLayout(attribute, image))
-        }
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% inlineInputMode=inline
+    //% block="add $layout image $image to current row repeat number attribute $attribute times"
+    export function createLayoutRepeatedImageZone(layout: CardLayoutTemplate, attribute: string, image: Image) {
+        addLayoutColumn(layout, cardKit.createAttributeAsRepeatImageLayout(attribute, image))
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% inlineInputMode=inline
-        //% block="add $this attribute $attribute as text in $color to current row|| limit line length $charsPerLine max lines $maxLines"
-        //% color.shadow="colorindexpicker" color.defl=15
-        //% charsPerLine.defl=5 maxLines.defl=1
-        createAttributeTextZone(attribute: string, color: number, charsPerLine: number, maxLines: number) {
-            this.addColumn(cardKit.createAttributeAsPlainTextLayout(attribute, color, charsPerLine, maxLines))
-        }
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% inlineInputMode=inline
+    //% block="add $layout attribute $attribute as text in $color to current row|| limit line length $charsPerLine max lines $maxLines"
+    //% color.shadow="colorindexpicker" color.defl=15
+    //% charsPerLine.defl=5 maxLines.defl=1
+    export function createLayoutAttributeTextZone(layout: CardLayoutTemplate, attribute: string, color: number, charsPerLine: number, maxLines: number) {
+        addLayoutColumn(layout, cardKit.createAttributeAsPlainTextLayout(attribute, color, charsPerLine, maxLines))
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% block="$this add text to current row| in $color use number attribute $attribute to index value from $textLookupTable|| limit line length $charsPerLine max lines $maxLines"
-        //% color.shadow="colorindexpicker" color.defl=15
-        //% charsPerLine.defl=5 maxLines.defl=1
-        createAttributeNumberToTextZone(attribute: string, textLookupTable: string[], color: number, charsPerLine: number, maxLines: number) {
-            this.addColumn(cardKit.createAttributeAsLookupTextLayout(attribute, color, charsPerLine, maxLines, cardKit.createNumberToTextLookupTable(textLookupTable)))
-        }
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% block="$layout add text to current row| in $color use number attribute $attribute to index value from $textLookupTable|| limit line length $charsPerLine max lines $maxLines"
+    //% color.shadow="colorindexpicker" color.defl=15
+    //% charsPerLine.defl=5 maxLines.defl=1
+    export function createLayoutAttributeNumberToTextZone(layout: CardLayoutTemplate, attribute: string, textLookupTable: string[], color: number, charsPerLine: number, maxLines: number) {
+        addLayoutColumn(layout, cardKit.createAttributeAsLookupTextLayout(attribute, color, charsPerLine, maxLines, cardKit.createNumberToTextLookupTable(textLookupTable)))
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% block="$this add image to current row| use number attribute $attribute to index value from $imageLookupTable"
-        //% imageLookupTable.shadow="lists_create_with" imageLookupTable.defl="screen_image_picker"
-        createAttributeNumberToImageZone(attribute: string, imageLookupTable: Image[]) {
-            this.addColumn(cardKit.createAttributeAsLookupImageLayout(attribute, cardKit.createNumberToImageLookupTable(imageLookupTable)))
-        }
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% block="$layout add image to current row| use number attribute $attribute to index value from $imageLookupTable"
+    //% imageLookupTable.shadow="lists_create_with" imageLookupTable.defl="screen_image_picker"
+    export function createLayoutAttributeNumberToImageZone(layout: CardLayoutTemplate, attribute: string, imageLookupTable: Image[]) {
+        addLayoutColumn(layout, cardKit.createAttributeAsLookupImageLayout(attribute, cardKit.createNumberToImageLookupTable(imageLookupTable)))
+    }
 
-        //% group="Layout" blockSetVariable="myCardLayout"
-        //% block="$this add image to current row| use text attribute $attribute and change $lookupTable"
-        //% lookupTable.shadow="lists_create_with" lookupTable.defl="textToImageLookupPicker"
-        createAttributeTextToImageZone(attribute: string, lookupTable: cardKit.LayoutLookup[]) {
-            this.addColumn(cardKit.createAttributeAsLookupImageLayout(attribute, lookupTable))
-        }
-
+    //% group="Layout" blockSetVariable="myCardLayout"
+    //% block="$layout add image to current row| use text attribute $attribute and change $lookupTable"
+    //% lookupTable.shadow="lists_create_with" lookupTable.defl="textToImageLookupPicker"
+    export function createLayoutAttributeTextToImageZone(layout: CardLayoutTemplate, attribute: string, lookupTable: cardKit.LayoutLookup[]) {
+        addLayoutColumn(layout, cardKit.createAttributeAsLookupImageLayout(attribute, lookupTable))
     }
 }
